7f5b2be8e869219004c58862c934d6ef3c12c2aa
tilly <rctillotson25@gmail.com>
2015-01-20 19:01:17 -0600

    Updated reverseString function and uniqueCharacters function to improve commenting and overall code structure.

--- src/playground/reverseString.go
+++ src/playground/reverseString.go
@@ -1,20 +1,29 @@
+/*
+
+   This command-line utility takes a line of input from the console
+   and prints out the reversed version of the string.
+
+   Created 1/15/2015 by rctillotson25
+
+
+*/
+
 package main
 
 import (
-	"fmt"
-	"utils"
-	"os"
 	"bufio"
+	"fmt"
+	"os"
+	"utils"
 )
 
-
-// TODO -Improve the trimming of strings so that it doesn't pick up the \n at the end. 
+// TODO - Improve the trimming of strings so that it doesn't pick up the \n at the end.
 
 func main() {
 
 	s := getInput()
 
-	fmt.Println("String: \t\t",  s)
+	fmt.Println("String: \t\t", s)
 	fmt.Println("Reversed String: \t", reverseString(s))
 
 	os.Exit(0)
@@ -25,6 +34,7 @@ func reverseString(s string) string {
 	var high int = len(s) - 1
 	var strArr []byte
 
+	// strings are read-only slices in Go - must copy string to swap
 	strArr = make([]byte, len(s))
 	copy(strArr, s)
 
@@ -38,7 +48,7 @@ func reverseString(s string) string {
 
 func getInput() string {
 	var s string
-	
+
 	console := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter a string to be reversed: ")
 	s, err := console.ReadString('\n')
--- src/playground/uniqueCharacters.go
+++ src/playground/uniqueCharacters.go
@@ -1,3 +1,12 @@
+/*
+
+   This command-line application gets the user input and determines
+   if the string entered has all unique characters.
+
+   Authored 1/17/2015  by Ryan Tillotson
+
+*/
+
 package main
 
 import (
@@ -8,6 +17,35 @@ import (
 )
 
 func main() {
+
+	input := getInput()
+
+	result := UniqueString(input)
+
+	fmt.Println("Result: ", result)
+
+}
+
+/*
+   Base case:
+   	Empty string is unique and indicates all characters unique
+   Failure case:
+        Tail contains the head of the string
+   Recursive case:
+        Check the tail of the string
+
+*/
+func UniqueString(s string) bool {
+	if len(s) <= 0 {
+		return true
+	} else if strings.Contains(string(s[1:]), string(s[0])) == true {
+		return false
+	} else {
+		return UniqueString(s[1:])
+	}
+}
+
+func getInput() string {
 	console := bufio.NewReader(os.Stdin)
 	fmt.Println("This command-line utility determines if a string has unique characters.")
 	fmt.Println("Please enter a string: ")
@@ -19,18 +57,5 @@ func main() {
 		os.Exit(1)
 	}
 
-	result := UniqueString(input)
-
-	fmt.Println("Result: ", result)
-
-}
-
-func UniqueString(s string) bool {
-	if len(s) <= 0 {
-		return true
-	} else if strings.Contains(string(s[1:]), string(s[0])) == true {
-		return false
-	} else {
-		return UniqueString(s[1:])
-	}
+	return input
 }
