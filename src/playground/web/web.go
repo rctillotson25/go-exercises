463d02e02bb94cc860fbeb7546fbde455a25c51e
Ryan <rctillotson25@gmail.com>
2015-01-27 15:18:51 -0600

    New basic http web app example.

---
+++ src/playground/web/web.go
@@ -0,0 +1,25 @@
+/*
+
+   Basic web app that prints output
+   based on the path.
+
+   Authored on 1/27/2015 by rctillotson25
+
+
+*/
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
